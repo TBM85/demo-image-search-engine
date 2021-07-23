1c112309a5d2d7e5804a72cd1dbd696d087633bb
TBM85 <tballesterm@gmail.com>
2021-07-23 09:14:51 -0400

    Enlarge the clicked image

--- src/components/ImageOutput/ImageOutput.js
+++ src/components/ImageOutput/ImageOutput.js
@@ -1,20 +1,47 @@
-import { Container, ImageListItem }  from '@material-ui/core';
+import { useState } from "react";
+import {
+  Container,
+  Dialog,
+  ImageList,
+  ImageListItem,
+} from "@material-ui/core";
 
 import classes from "./ImageOutput.module.scss";
-import { ImageList } from "@material-ui/core";
 
 const ImageOutput = (props) => {
   const { images } = props;
+
+  const [selectedImg, setSelectedImg] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = (imgSelected) => {
+    setIsOpen(true);
+    setSelectedImg(imgSelected);
+  };
 
   return (
     <Container className={classes.ImageContainer}>
       <ImageList cols={4} className={classes.ImageList}>
         {images.map((img) => (
           <ImageListItem key={img.id} className={classes.ImageListItem}>
-            <img src={img.largeImageURL} alt={img.tags} />
+            <img
+              src={img.largeImageURL}
+              alt={img.tags}
+              onClick={() => handleOpen(img.largeImageURL)}
+            />
           </ImageListItem>
         ))}
       </ImageList>
+
+      <Dialog
+        className={classes.Dialog}
+        modal="true"
+        open={isOpen}
+        maxWidth="md"
+      >
+        <img src={selectedImg} alt="" />
+      </Dialog>
+
     </Container>
   );
 };
