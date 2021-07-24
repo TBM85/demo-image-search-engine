cba424100b0c45d9b41167b5d022411d303cba92
TBM85 <tballesterm@gmail.com>
2021-07-24 09:31:04 -0400

    Show image's author when the info icon is clicked

--- src/components/ImageOutput/ImageOutput.js
+++ src/components/ImageOutput/ImageOutput.js
@@ -3,9 +3,10 @@ import {
   Container,
   Dialog,
   ImageList,
-  ImageListItem,
+  ImageListItem
 } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
+import InfoIcon from "@material-ui/icons/Info";
 
 import classes from "./ImageOutput.module.scss";
 
@@ -13,15 +14,25 @@ const ImageOutput = (props) => {
   const { images } = props;
 
   const [selectedImg, setSelectedImg] = useState(null);
+  const [selectedAlt, setSelectedAlt] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
+  const [showInfo, setShowInfo] = useState(false);
 
-  const handleOpen = (imgSelected) => {
+  const handleOpen = (imgSelected, imgAlt, imgUser) => {
     setIsOpen(true);
     setSelectedImg(imgSelected);
+    setSelectedAlt(imgAlt);
+    setSelectedUser(imgUser)
   };
 
   const handleClose = () => {
     setIsOpen(false);
+    setShowInfo(false);
+  };
+
+  const handleInfo = () => {
+    setShowInfo(!showInfo);
   };
 
   return (
@@ -32,7 +43,7 @@ const ImageOutput = (props) => {
             <img
               src={img.largeImageURL}
               alt={img.tags}
-              onClick={() => handleOpen(img.largeImageURL)}
+              onClick={() => handleOpen(img.largeImageURL, img.tags, img.user)}
             />
           </ImageListItem>
         ))}
@@ -46,7 +57,13 @@ const ImageOutput = (props) => {
         maxWidth="md"
       >
         <CloseIcon onClick={handleClose} className={classes.CloseIcon} />
-        <img src={selectedImg} alt="" />
+        <img src={selectedImg} alt={selectedAlt} />
+        <InfoIcon onClick={handleInfo} className={classes.InfoIcon} />
+        {showInfo && (
+          <div className={classes.InfoContainer}>
+            <p>Author: {selectedUser}</p>
+          </div>
+        )}
       </Dialog>
     </Container>
   );
